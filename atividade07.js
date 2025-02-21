253c57f32905394a93155a0c61a0c9ccee3b8166
Gabi08-l <senaioloko@gmail.com>
2025-02-21 10:21:27 -0300

    Atualizações

--- atividade07.js
+++ atividade07.js
@@ -11,7 +11,34 @@ var matriz = [
     [1, 9, 2],
     [7, 1, 4],
   ];
-  console.table(matriz);
-console.log(matriz[0][0], "|", matriz[1][1], "|", matriz[2][2]);
-//for (let linha = 0; linha < matriz.length; linha++) {
-  //  for (let coluna = 0; coluna < matriz[0].length; coluna++)
+  for (let linha = 0; linha < matriz.length; linha++) {
+    for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+        linha == coluna &&
+       console.log(`Matriz [${linha}][${coluna}] = ${matriz[linha][coluna]}`);
+    }
+  }
+
+console.log("====================================================================")
+
+  //Dada a matriz abaixo, faça um programa em javascript que multiplique seus elementos por um valor x
+  //e exiba ao usuario a matriz antes e depois após a multiplicação
+//|3 5 8 |
+//|1 9 2 |
+//|7 1 4 |
+
+var x = 4
+var matriz = [
+    [3, 5, 8],
+    [1, 9, 2],
+    [7, 1, 4],
+];
+console.table(matriz);
+
+for (let linha = 0; linha.length; linha++){
+    for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+matriz[linha][coluna] *= x
+    }
+    }
+console.table(matriz);
+
+//Faça num loop FOR onde apresente os numeros impares de 0 a 100.
